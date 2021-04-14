9b91a6c454fed8dbda4b29532724db1ba9d96913
Michael Dorst <m@mdorst.net>
2021-04-14 16:27:46 -0700

    Add get_branches()

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use chrono::prelude::*;
+use git2::{BranchType, Repository};
+
 fn main() -> Result<()> {
     let repo = git2::Repository::open_from_env()?;
 
-    let local_branches = list_branches(&repo, git2::BranchType::Local)?;
-    let remote_branches = list_branches(&repo, git2::BranchType::Remote)?;
+    let local_branches = list_branches(&repo, Some(BranchType::Local))?;
+    let remote_branches = list_branches(&repo, Some(BranchType::Remote))?;
     println!("Local branches:");
     println!("{}", local_branches);
     println!("Remote branches:");
@@ -11,16 +14,37 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn list_branches(repo: &git2::Repository, branch_type: git2::BranchType) -> Result<String> {
-    let mut branches = String::new();
-    for branch in repo.branches(Some(branch_type))? {
-        let (branch, _) = branch?;
-        let name = String::from_utf8_lossy(branch.name_bytes()?);
-        let commit_oid = branch.get().peel_to_commit()?.id();
-        let commit_sha1 = format!("{}", commit_oid);
-        branches.push_str(&format!("{} ({})\n", name, &commit_sha1[0..7]));
-    }
-    Ok(branches)
+fn get_branches(repo: &Repository, branch_type: Option<BranchType>) -> Result<Vec<Branch>> {
+    repo.branches(branch_type)?
+        .map(|branch| {
+            let (branch, _) = branch?;
+            let commit = branch.get().peel_to_commit()?;
+            let time = commit.time();
+            Ok(Branch {
+                name: String::from_utf8_lossy(branch.name_bytes()?).into(),
+                commit_id: format!("{}", commit.id()),
+                time: NaiveDateTime::from_timestamp(
+                    time.seconds() + (time.offset_minutes() as i64),
+                    0,
+                ),
+            })
+        })
+        .collect()
+}
+
+fn list_branches(repo: &Repository, branch_type: Option<BranchType>) -> Result<String> {
+    let branches = get_branches(repo, branch_type)?
+        .into_iter()
+        .map(|branch| Ok(format!("{} ({})\n", branch.name, &branch.commit_id[0..7])))
+        .collect::<Result<Vec<_>>>()?;
+    Ok(branches.join(""))
+}
+
+#[derive(Debug)]
+struct Branch {
+    name: String,
+    commit_id: String,
+    time: NaiveDateTime,
 }
 
 #[derive(Debug, thiserror::Error)]

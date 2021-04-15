2f5b7c644bd83c8ac392b87e88198606b4a4b42a
Michael Dorst <m@mdorst.net>
2021-04-14 17:54:03 -0700

    Implement REPL

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,53 @@
 use chrono::prelude::*;
 use git2::{BranchType, Repository};
+use std::convert::TryFrom;
+use std::io;
+use std::io::{Read, Write};
 
 fn main() -> Result<()> {
-    let repo = git2::Repository::open_from_env()?;
-
-    let local_branches = list_branches(&repo, Some(BranchType::Local))?;
-    let remote_branches = list_branches(&repo, Some(BranchType::Remote))?;
-    println!("Local branches:");
-    println!("{}", local_branches);
-    println!("Remote branches:");
-    println!("{}", remote_branches);
-
+    let repo = Repository::open_from_env()?;
+    let mut stdout = io::stdout();
+    let mut stdin = io::stdin().bytes();
+    crossterm::terminal::enable_raw_mode()?;
+    'branch_loop: for branch in get_branches(&repo, Some(BranchType::Local))? {
+        loop {
+            write!(
+                stdout,
+                "\r\n{} ({}) last commit at {} (k/d/q/?) > ",
+                branch.name,
+                &branch.sha1[0..7],
+                branch.time
+            )?;
+            stdout.flush()?;
+            // Unwrapping is okay because stdin.next() never returns None.
+            let selection = stdin.next().unwrap()?;
+            let action = BranchAction::try_from(selection);
+            match action {
+                Ok(BranchAction::Keep) => {
+                    write!(stdout, "Keeping {}\r\n", branch.name)?;
+                }
+                Ok(BranchAction::Delete) => {
+                    write!(stdout, "Deleting {}\r\n", branch.name)?;
+                    unimplemented!();
+                }
+                Ok(BranchAction::Help) => {
+                    write!(stdout, "Help:\r\n")?;
+                    write!(stdout, "k - Keep the branch\r\n")?;
+                    write!(stdout, "d - Delete the branch\r\n")?;
+                    write!(stdout, "q - Quit\r\n")?;
+                    write!(stdout, "? - Show this help menu\r\n")?;
+                }
+                Ok(BranchAction::Quit) => {
+                    write!(stdout, "Quitting\r\n")?;
+                    break 'branch_loop;
+                }
+                Err(e) => {
+                    write!(stdout, "{}\r\n", e)?;
+                }
+            }
+        }
+    }
+    crossterm::terminal::disable_raw_mode()?;
     Ok(())
 }
 
@@ -23,7 +60,7 @@ fn get_branches(repo: &Repository, branch_type: Option<BranchType>) -> Result<Ve
             let time = commit.time();
             Ok(Branch {
                 name: String::from_utf8_lossy(branch.name_bytes()?).into(),
-                commit_id: format!("{}", commit.id()),
+                sha1: format!("{}", commit.id()),
                 time: NaiveDateTime::from_timestamp(
                     time.seconds() + (time.offset_minutes() as i64),
                     0,
@@ -35,19 +72,39 @@ fn get_branches(repo: &Repository, branch_type: Option<BranchType>) -> Result<Ve
     Ok(branches)
 }
 
-fn list_branches(repo: &Repository, branch_type: Option<BranchType>) -> Result<String> {
-    let branches = get_branches(repo, branch_type)?
-        .into_iter()
-        .map(|branch| Ok(format!("{} ({})\n", branch.name, &branch.commit_id[0..7])))
-        .collect::<Result<Vec<_>>>()?;
-    Ok(branches.join(""))
-}
-
 #[derive(Debug)]
 struct Branch {
     name: String,
-    commit_id: String,
+    sha1: String,
     time: NaiveDateTime,
+}
+
+enum BranchAction {
+    Keep,
+    Delete,
+    Quit,
+    Help,
+}
+
+impl TryFrom<char> for BranchAction {
+    type Error = Error;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'k' => Ok(BranchAction::Keep),
+            'd' => Ok(BranchAction::Delete),
+            'q' => Ok(BranchAction::Quit),
+            '?' => Ok(BranchAction::Help),
+            _ => Err(Error::InvalidInput(value)),
+        }
+    }
+}
+
+impl TryFrom<u8> for BranchAction {
+    type Error = Error;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        BranchAction::try_from(char::from(value))
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -56,6 +113,10 @@ enum Error {
     CrosstermError(#[from] crossterm::ErrorKind),
     #[error(transparent)]
     Git2Error(#[from] git2::Error),
+    #[error(transparent)]
+    IoError(#[from] io::Error),
+    #[error("Invalid input `{0}`")]
+    InvalidInput(char),
 }
 
 type Result<T, E = Error> = std::result::Result<T, E>;
